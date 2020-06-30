abeb3f63453de23c5216a0d82c4e36d73a3820d8
Juan Esteban <ospinajuan2000@gmail.com>
2020-06-30 16:21:34 -0500

    mensaje

---
+++ docs/assets/js/peticiones.js
@@ -0,0 +1,19 @@
+const gitHubUser = `https://api.github.com/users/`;
+
+const getInfoUser = async (user) => {
+
+    const response = await fetch(`${gitHubUser}${user}`);
+
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    } else {
+        throw 'Error';
+    }
+}
+
+
+
+export {
+    getInfoUser,
+}
